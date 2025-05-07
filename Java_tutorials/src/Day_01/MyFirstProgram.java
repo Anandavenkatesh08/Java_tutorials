6d32d7093a4ecc7782b179f8deccf021da9cd380
Anandavenkatesh08 <av512605@gmail.com>
2025-05-08 01:53:25 +0530

    MY Day 02 programs

--- Java_tutorials/src/Day_01/MyFirstProgram.java
+++ Java_tutorials/src/Day_01/MyFirstProgram.java
@@ -4,7 +4,7 @@ public class MyFirstProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hi Anand once completing your code in eclipse move to git");
 	}
 
 }
---
+++ Java_tutorials/src/Day_02/Additionoftwonumbers.java
@@ -0,0 +1,15 @@
+package Day_02;
+
+public class Additionoftwonumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.println("The sum is: " + sum);
+
+	}
+
+}

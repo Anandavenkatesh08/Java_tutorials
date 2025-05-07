00af2657689942b6cbca70597f7fa78d401c052c
Anandavenkatesh08 <av512605@gmail.com>
2025-05-08 02:12:20 +0530

    day 02 remaining program

---
+++ Java_tutorials/src/Day_02/DatatypesandVariables.java
@@ -0,0 +1,26 @@
+package Day_02;
+
+public class DatatypesandVariables {
+	// TODO Auto-generated method stub
+		 // Instance variable
+	    int instanceVar = 50;
+
+	    // Static variable
+	    static int staticVar = 100;
+
+	    public static void main(String[] args) {
+	        // Local variable
+	        int localVar = 25;
+	        float pi = 3.14f;
+	        char grade = 'A';
+	        boolean passed = true;
+
+	        System.out.println("Local Variable: " + localVar);
+	        System.out.println("Static Variable: " + staticVar);
+	        System.out.println("Float Value: " + pi);
+	        System.out.println("Character Grade: " + grade);
+	        System.out.println("Passed?: " + passed);
+
+	}
+
+}

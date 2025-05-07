fd72a39f0b843ddd43a8acdcc06fef88e7c0cf55
Anandavenkatesh08 <av512605@gmail.com>
2025-05-08 03:11:59 +0530

    day_03 operators

---
+++ Java_tutorials/src/Day_03/OperatorsAndExpressions.java
@@ -0,0 +1,32 @@
+package Day_03;
+
+public class OperatorsAndExpressions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30,b=20;
+		//Assignment Operators
+		System.out.println("Sum of a and b is:"+(a+b));
+		System.out.println("Sum of a and b is:"+(a-b));
+		System.out.println("Sum of a and b is:"+(a*b));
+		System.out.println("Sum of a and b is:"+(a/b));
+		System.out.println("Sum of a and b is:"+(a%b));
+		
+		//Relational Operators
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+	     b=30;
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		
+		System.out.println(a!=b);
+		System.out.println(a==b);
+		
+		boolean res=a>b;
+		System.out.println(res);
+
+	}
+
+}
